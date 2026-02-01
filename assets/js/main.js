41b905c301b7a8108afc775be2a7862949ed0532
shashi7679 <shashikantprasad1111@gmail.com>
2026-02-01 17:49:56 +0900

    #Updated on 2026-02-01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,117 +1,194 @@
-/*
-	Strata by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+/*=============== HOME SPLIT TEXT ===============*/
+const { animate, text, stagger } = anime
+const { chars: chars1 } = text.split('.home__professional-1', {chars: true})
+const { chars: chars2 } = text.split('.home__professional-2', {chars: true})
 
-(function($) {
+animate(chars1, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 4000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
 
-	var $window = $(window),
-		$body = $('body'),
-		$header = $('#header'),
-		$footer = $('#footer'),
-		$main = $('#main'),
-		settings = {
+animate(chars2, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 4000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
 
-			// Parallax background effect?
-				parallax: true,
+/*=============== SWIPER PROJECTS ===============*/
+const swiperProjects = new Swiper('.projects__swiper', {
+  loop: true,
+  spaceBetween: 24,
+  slidesPerView: 'auto',
+  grabCursor: true,
+  speed: 600,
+  
 
-			// Parallax factor (lower = more intense, higher = less intense).
-				parallaxFactor: 20
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
-		};
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  }
+});
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1800px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ '481px',   '736px'  ],
-			xsmall:  [ null,      '480px'  ],
-		});
+/*=============== WORK TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+tabs.forEach(tab =>{
+  tab.addEventListener('click', () =>{
+    const targetSelector = tab.dataset.target,
+          targetContent = document.querySelector(targetSelector)
 
-	// Touch?
-		if (browser.mobile) {
+          //Disable all content and active tabs
+    tabContents.forEach((content)=>content.classList.remove('work-active'))
+    tabs.forEach((t)=>t.classList.remove('work-active'))
 
-			// Turn on touch mode.
-				$body.addClass('is-touch');
+    //Activate target content and tab
+    targetContent.classList.add('work-active')
+    tab.classList.add('work-active')
 
-			// Height fix (mostly for iOS).
-				window.setTimeout(function() {
-					$window.scrollTop($window.scrollTop() + 1);
-				}, 0);
 
-		}
+  })
+})
 
-	// Footer.
-		breakpoints.on('<=medium', function() {
-			$footer.insertAfter($main);
-		});
 
-		breakpoints.on('>medium', function() {
-			$footer.appendTo($header);
-		});
+/*=============== SERVICES ACCORDION ===============*/
+const servicesButtons = document.querySelectorAll('.services__button')
 
-	// Header.
+servicesButtons.forEach(button =>{
+  const heightInfo = document.querySelector('.services__info')
+  heightInfo.style.height = heightInfo.scrollHeight + "px"
+  
+  button.addEventListener('click', () =>{
+    const servicesCards = document.querySelectorAll('.services__card'),
+          currentCard = button.parentNode,
+          currentInfo = currentCard.querySelector('.services__info')
+          isCardOpen = currentCard.classList.contains('services-open')
 
-		// Parallax background.
+    servicesCards.forEach(card =>{
+      card.classList.replace('services-open', 'services-close')
 
-			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-				if (browser.name == 'ie'
-				||	browser.mobile)
-					settings.parallax = false;
+      const info = card.querySelector('.services__info')
+      info.style.height = '0'
+    })
 
-			if (settings.parallax) {
+    if(!isCardOpen){
+      currentCard.classList.replace('services-close', 'services-open')
+      currentInfo.style.height = currentInfo.scrollHeight + "px"
+    }
+    
+})
 
-				breakpoints.on('<=medium', function() {
+})
 
-					$window.off('scroll.strata_parallax');
-					$header.css('background-position', '');
+/*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/
 
-				});
 
-				breakpoints.on('>medium', function() {
+/*=============== COPY EMAIL IN CONTACT ===============*/
+const copyBtn = document.getElementById('contact-btn'),
+      copyEmail = document.getElementById('contact-email').textContent
 
-					$header.css('background-position', 'left 0px');
+copyBtn.addEventListener('click', ()=> {
+  navigator.clipboard.writeText(copyEmail).then(() =>{
+    copyBtn.innerHTML = 'Email Copied <i class="ri-check-line"></i>'
 
-					$window.on('scroll.strata_parallax', function() {
-						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-					});
 
-				});
+    setTimeout(() => {
+      copyBtn.innerHTML = 'Copy Email <i class="ri-file-copy-line"></i>'
+    }, 2000)
+  })
+})
 
-				$window.on('load', function() {
-					$window.triggerHandler('scroll');
-				});
 
-			}
+/*=============== CURRENT YEAR OF THE FOOTER ===============*/ 
+const textYear = document.querySelector('.footer-year'),
+    currentYear = new Date().getFullYear()
 
-	// Main Sections: Two.
+if(textYear) {
+  textYear.textContent = currentYear
+}
 
-		// Lightbox gallery.
-			$window.on('load', function() {
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
-				$('#two').poptrox({
-					caption: function($a) { return $a.next('h3').text(); },
-					overlayColor: '#2c2c2c',
-					overlayOpacity: 0.85,
-					popupCloserText: '',
-					popupLoaderText: '',
-					selector: '.work-item a.image',
-					usePopupCaption: true,
-					usePopupDefaultStyling: false,
-					usePopupEasyClose: false,
-					usePopupNav: true,
-					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
-				});
 
-			});
+const scrollActive = () => {
+  const scrollY = window.scrollY
 
-})(jQuery);
+  sections.forEach(section => {
+    const id = section.id,
+        top = section.offsetTop - 50,
+        height = section.offsetHeight,
+        link = document.querySelector('.nav__menu a[href*=' + id + ']')
+
+    if(!link) return
+
+
+    link.classList.toggle('active-link', scrollY > top && scrollY <= top + height)
+  })
+}
+
+/*=============== CUSTOM CURSOR ===============*/
+const cursor = document.querySelector('.cursor')
+let mouseX = 0, mouseY = 0
+
+const cursorMove = () => {
+  cursor.style.left = `${mouseX}px`
+  cursor.style.top = `${mouseY}px`
+  cursor.style.transform = 'translate(-50%, -50%)'
+
+  requestAnimationFrame(cursorMove)
+}
+
+
+document.addEventListener('mousemove', (e) => {
+  mouseX = e.clientX
+  mouseY = e.clientY
+})
+
+cursorMove()
+/* Hide custom cursor on links */
+const a = document.querySelectorAll('a')
+
+a.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('hide-cursor')
+  })
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('hide-cursor')
+  })
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2000,
+  delay: 300,
+  // reset: true,
+}
+)
+
+sr.reveal('.home__image, .projects__container, .work__container, .work__container')
+sr.reveal('.home__data', {delay: 900, origin: 'bottom'})
+sr.reveal('.home__info', {delay: 1200, origin: 'bottom'})
+sr.reveal('.home__social, .home__cv', {delay: 1500, origin: 'bottom'})
+sr.reveal('.about__data', {origin: 'left'})
+sr.reveal('.about__image', {origin: 'right'})
+sr.reveal('.services__card', {interval: 100})
